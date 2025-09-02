baaf8a1cbf927423f267a6fbcc3db28ad42b0e6b
gorfront <avakimyan.gor.00@gmail.com>
2025-09-02 10:46:31 +0400

    Add CORS middleware to your backend

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import taskRoutes from './routes/taskRoutes';
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }))
 app.use(express.json());
 
 app.use(taskRoutes);
